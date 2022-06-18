fe2cf3a1a0b83e2e009e4e94588b7160ba94c1f8
Mohd Faraz <androiabledroid@gmail.com>
2022-06-18 22:51:55 +0200

    Set /data/recovery for Settings
     * Test: TWRP boots, settings saves, Change TWRP Folder works
    
    Change-Id: Iaae45064815a4adc55e1116f2c51fba869b284b5
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- gui/action.cpp
+++ gui/action.cpp
@@ -1130,7 +1130,7 @@ int GUIAction::wipe(std::string arg)
 			if (has_datamedia) {
 				ret_val = PartitionManager.Wipe_Media_From_Data();
 			} else {
-				ret_val = PartitionManager.Wipe_By_Path(DataManager::GetSettingsStoragePath());
+				ret_val = PartitionManager.Wipe_By_Path(DataManager::GetCurrentStoragePath());
 			}
 		} else if (arg == "EXTERNAL") {
 			string External_Path;
@@ -1180,7 +1180,7 @@ int GUIAction::wipe(std::string arg)
 							gui_msg(Msg(msg::kError, "unable_to_wipe=Unable to wipe {1}.")(wipe_path));
 							ret_val = false;
 							break;
-						} else if (wipe_path == DataManager::GetSettingsStoragePath()) {
+						} else if (wipe_path == DataManager::GetCurrentStoragePath()) {
 							arg = wipe_path;
 						}
 					} else {
@@ -2327,7 +2327,7 @@ int GUIAction::editfile(std::string arg) {
 int GUIAction::applycustomtwrpfolder(string arg __unused)
 {
 	operation_start("ChangingTWRPFolder");
-	string storageFolder = DataManager::GetSettingsStoragePath();
+	string storageFolder = DataManager::GetCurrentStoragePath();
 	string newFolder = storageFolder + '/' + arg;
 	string newBackupFolder = newFolder + "/BACKUPS/" + DataManager::GetStrValue("device_id");
 	string prevFolder = storageFolder + DataManager::GetStrValue(TW_RECOVERY_FOLDER_VAR);
@@ -2351,7 +2351,6 @@ int GUIAction::applycustomtwrpfolder(string arg __unused)
 	if (ret) {
 		DataManager::SetValue(TW_RECOVERY_FOLDER_VAR, '/' + arg);
 		DataManager::SetValue(TW_BACKUPS_FOLDER_VAR, newBackupFolder);
-		DataManager::mBackingFile = newFolder + '/' + TW_SETTINGS_FILE;
 	}
 	operation_end((int)!ret);
 	return 0;
